78cddc48efa26fd7a39b703737b35a3c7e7d53dc
valiyevvali <valiyevveli@outlook.com>
2023-05-15 16:25:11 +0400

    lint errors fixed

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,6 @@ from core import models
 from unittest.mock import patch
 
 
-
 def create_user(email='user@example.com', password='testpass123'):
     """Create a return a new user."""
     return get_user_model().objects.create_user(email, password)
