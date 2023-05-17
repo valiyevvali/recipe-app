670b463d421bc95e05eb7ea34c7ec26b455660d3
valiyevvali <valiyevveli@outlook.com>
2023-05-17 17:52:57 +0400

    deploy configuration added

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,13 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p8ixlg#)swpb5m@xvr4+my8!m9!dd6bg4*agocg5d5ozdt6t#8'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 # Application definition
 
 INSTALLED_APPS = [
